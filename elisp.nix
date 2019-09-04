9653b9a21bd69b71f23c00c1043e2002cc9809ac
Piotr Limanowski <piotr@codearsonist.com>
2019-09-04 10:00:05 +0200

    workaround for use-package with builtin packages
    this is crap and makes little sense but I just don't want to edit
    my emacs config now.

--- elisp.nix
+++ elisp.nix
@@ -20,13 +20,14 @@ let
 
   parsePackages = dotEmacs: let
     strippedComments = stripComments dotEmacs;
+    emacsBuiltinPackages =  with pkgs.lib; splitString "\n" (strings.fileContents ./repos/builtin/generated);
     tokens = builtins.filter (t: !(isStrEmpty t)) (builtins.map
       (t: if builtins.typeOf t == "list" then builtins.elemAt t 0 else t)
       (builtins.split "([\(\)])" strippedComments));
     matches = builtins.map (t:
       builtins.match "^use-package[[:space:]]+([A-Za-z0-9_-]+).*" t) tokens;
   in builtins.map (m: builtins.elemAt m 0)
-      (builtins.filter (m: m != null) matches);
+      (builtins.filter (m: m != null && emacsBuiltinPackages ? m) matches);
 
 in {
   config,
